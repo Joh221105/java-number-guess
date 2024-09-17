8374aa5a638993bfaab070e24d32148c0f3f4697
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-16 20:22:23 -0400

    generates a random number between 0 and 100 for user to guess

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,20 @@
 package src;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+        int target = generateRandomNumber();
+    }
+
+    public static int generateRandomNumber(){
+        Random rand = new Random();
+        return rand.nextInt(101);
+    }
+
+    public static void guessingGame(int target){
+
     }
 }
