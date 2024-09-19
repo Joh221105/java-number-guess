e78e024e21559c4467a7577371a269447dc3fb03
Joh221105 <101863325+Joh221105@users.noreply.github.com>
2024-09-18 20:46:02 -0400

    implements function to validate user input

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ public class Main {
         Scanner scan = new Scanner(System.in);
         int target = generateRandomNumber();
         guessingGame(scan, target);
-        System.out.print("You got it! The number was: " + target);
     }
 
     public static int generateRandomNumber(){
@@ -16,18 +15,35 @@ public class Main {
         return rand.nextInt(101);
     }
 
-    public static void guessingGame(Scanner scan, int target){
+    public static int getValidInput(Scanner scan) {
         int guess = -1;
-        do{
+        boolean valid = false;
+        while (!valid) {
             System.out.print("Enter a number between 0 and 100: ");
-            guess = scan.nextInt();
-            if(guess > target){
-                System.out.println("It is too high");
-            }
-            else if(guess < target){
-                System.out.println("It is too low");
+            if (scan.hasNextInt()) {
+                guess = scan.nextInt();
+                if (guess >= 0 && guess <= 100) {
+                    valid = true;
+                } else {
+                    System.out.println("Please enter a number between 0 and 100.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter an integer.");
+                scan.next();
             }
         }
-        while(guess != target);
+        return guess;
+    }
+
+    public static void guessingGame(Scanner scan, int target){
+        int guess;
+        do {
+            guess = getValidInput(scan);
+            if (guess > target) {
+                System.out.println("It is too high");
+            } else if (guess < target) {
+                System.out.println("It is too low");
+            }
+        } while (guess != target);
     }
 }
